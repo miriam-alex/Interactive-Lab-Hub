928026c6d0e67a73f46f30f918c7dcac58e0c4b5
Miriam Alex <mta64@cornell.edu>
2025-09-14 13:38:44 -0400

    Part D initial screen_clock.py setup

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.D5) 
@@ -61,11 +62,14 @@ backlight.switch_to_output()
 backlight.value = True
 
 while True:
+
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
+    time_string = strftime("%m/%d/%Y %H:%M:%S")
+    y = top
+    draw.text((x, y), time_string, font=font, fill="#FFFFFF")
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
